35bad73e2bdef38a507a67dfe623623df83b50d7
Alejandra <ai2733@gadi-login-05.gadi.nci.org.au>
2026-03-31 17:04:00 +1100

    added codes to run figures, post-processed data and figures

---
+++ data/arrays/copy_files.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+DST="/g/data/up6/ai2733/Gusts_downdrafts/data/arrays"
+mkdir -p $DST
+
+cp /g/data/w28/ai2733/arrays/CASE1_d02_waAll_80kmAWS_coarsen_Alldims.npy $DST/
+cp /g/data/w28/ai2733/arrays/CASE1_d02_QRAINAll_80kmAWS_coarsen_Alldims.npy $DST/
+cp /g/data/w28/ai2733/arrays/CASE1_d02_QGRAUPAll_80kmAWS_coarsen_Alldims.npy $DST/
+cp /g/data/w28/ai2733/arrays/CASE1_d02_QVAPORAll_80kmAWS_coarsen_Alldims.npy $DST/
+cp /g/data/w28/ai2733/arrays/CASE1_d02_tempAll_80kmAWS_coarsen_Alldims.npy $DST/
+cp /g/data/w28/ai2733/arrays/CASE1_d02_zAll_80kmAWS_coarsen_Alldims.npy $DST/
+
+cp /g/data/w28/ai2733/arrays/CASE1_d03_waAll_80kmAWS_coarsen_Alldims.npy $DST/
+cp /g/data/w28/ai2733/arrays/CASE1_d03_QRAINAll_80kmAWS_coarsen_Alldims.npy $DST/
+cp /g/data/w28/ai2733/arrays/CASE1_d03_QGRAUPAll_80kmAWS_coarsen_Alldims.npy $DST/
+cp /g/data/w28/ai2733/arrays/CASE1_d03_QVAPORAll_80kmAWS_coarsen_Alldims.npy $DST/
+cp /g/data/w28/ai2733/arrays/CASE1_d03_tempAll_80kmAWS_coarsen_Alldims.npy $DST/
+cp /g/data/w28/ai2733/arrays/CASE1_d03_zAll_80kmAWS_coarsen_Alldims.npy $DST/
+
+cp /g/data/w28/ai2733/arrays/CASE2_new_d02_waAll_80kmAWS_coarsen_Alldims.npy $DST/
+cp /g/data/w28/ai2733/arrays/CASE2_new_d02_QRAINAll_80kmAWS_coarsen_Alldims.npy $DST/
+cp /g/data/w28/ai2733/arrays/CASE2_new_d02_QGRAUPAll_80kmAWS_coarsen_Alldims.npy $DST/
+cp /g/data/w28/ai2733/arrays/CASE2_new_d02_QVAPORAll_80kmAWS_coarsen_Alldims.npy $DST/
+cp /g/data/w28/ai2733/arrays/CASE2_new_d02_tempAll_80kmAWS_coarsen_Alldims.npy $DST/
+cp /g/data/w28/ai2733/arrays/CASE2_new_d02_zAll_80kmAWS_coarsen_Alldims.npy $DST/
+
+cp /g/data/w28/ai2733/arrays/CASE2_new_d03_waAll_80kmAWS_coarsen_Alldims.npy $DST/
+cp /g/data/w28/ai2733/arrays/CASE2_new_d03_QRAINAll_80kmAWS_coarsen_Alldims.npy $DST/
+cp /g/data/w28/ai2733/arrays/CASE2_new_d03_QGRAUPAll_80kmAWS_coarsen_Alldims.npy $DST/
+cp /g/data/w28/ai2733/arrays/CASE2_new_d03_QVAPORAll_80kmAWS_coarsen_Alldims.npy $DST/
+cp /g/data/w28/ai2733/arrays/CASE2_new_d03_tempAll_80kmAWS_coarsen_Alldims.npy $DST/
+cp /g/data/w28/ai2733/arrays/CASE2_new_d03_zAll_80kmAWS_coarsen_Alldims.npy $DST/
+
+cp /g/data/w28/ai2733/arrays/CASE3_new_d02_waAll_80kmAWS_coarsen_Alldims.npy $DST/
+cp /g/data/w28/ai2733/arrays/CASE3_new_d02_QRAINAll_80kmAWS_coarsen_Alldims.npy $DST/
+cp /g/data/w28/ai2733/arrays/CASE3_new_d02_QGRAUPAll_80kmAWS_coarsen_Alldims.npy $DST/
+cp /g/data/w28/ai2733/arrays/CASE3_new_d02_QVAPORAll_80kmAWS_coarsen_Alldims.npy $DST/
+cp /g/data/w28/ai2733/arrays/CASE3_new_d02_tempAll_80kmAWS_coarsen_Alldims.npy $DST/
+cp /g/data/w28/ai2733/arrays/CASE3_new_d02_zAll_80kmAWS_coarsen_Alldims.npy $DST/
+
+cp /g/data/w28/ai2733/arrays/CASE3_new_d03_waAll_80kmAWS_coarsen_Alldims.npy $DST/
+cp /g/data/w28/ai2733/arrays/CASE3_new_d03_QRAINAll_80kmAWS_coarsen_Alldims.npy $DST/
+cp /g/data/w28/ai2733/arrays/CASE3_new_d03_QGRAUPAll_80kmAWS_coarsen_Alldims.npy $DST/
+cp /g/data/w28/ai2733/arrays/CASE3_new_d03_QVAPORAll_80kmAWS_coarsen_Alldims.npy $DST/
+cp /g/data/w28/ai2733/arrays/CASE3_new_d03_tempAll_80kmAWS_coarsen_Alldims.npy $DST/
+cp /g/data/w28/ai2733/arrays/CASE3_new_d03_zAll_80kmAWS_coarsen_Alldims.npy $DST/
+
+cp /g/data/w28/ai2733/arrays/TimeProf_CASE1_d02_pressure_80kmAWS_coarsen.npy $DST/
+cp /g/data/w28/ai2733/arrays/TimeProf_CASE1_d02_height_80kmAWS_coarsen.npy $DST/
+cp /g/data/w28/ai2733/arrays/TimeProf_CASE1_d03_pressure_80kmAWS_coarsen.npy $DST/
+cp /g/data/w28/ai2733/arrays/TimeProf_CASE1_d03_height_80kmAWS_coarsen.npy $DST/
+
+cp /g/data/w28/ai2733/arrays/TimeProf_CASE2_new_d02_pressure_80kmAWS_coarsen.npy $DST/
+cp /g/data/w28/ai2733/arrays/TimeProf_CASE2_new_d02_height_80kmAWS_coarsen.npy $DST/
+cp /g/data/w28/ai2733/arrays/TimeProf_CASE2_new_d03_pressure_80kmAWS_coarsen.npy $DST/
+cp /g/data/w28/ai2733/arrays/TimeProf_CASE2_new_d03_height_80kmAWS_coarsen.npy $DST/
+
+cp /g/data/w28/ai2733/arrays/TimeProf_CASE3_new_d02_pressure_80kmAWS_coarsen.npy $DST/
+cp /g/data/w28/ai2733/arrays/TimeProf_CASE3_new_d02_height_80kmAWS_coarsen.npy $DST/
+cp /g/data/w28/ai2733/arrays/TimeProf_CASE3_new_d03_pressure_80kmAWS_coarsen.npy $DST/
+cp /g/data/w28/ai2733/arrays/TimeProf_CASE3_new_d03_height_80kmAWS_coarsen.npy $DST/
+
+cp /g/data/w28/ai2733/arrays/Times_CASE1.npy $DST/
+cp /g/data/w28/ai2733/arrays/Times_CASE2_new.npy $DST/
+cp /g/data/w28/ai2733/arrays/Times_CASE3_new.npy $DST/
+
+echo "All listed files copied."
